28e47c2d1fbd89a02ac788ff501691fa6bf24aad
zanetamak <zaneta.zawislak@gmail.com>
2023-11-30 16:42:41 +0100

    app

---
+++ src/components/Api.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const KEY = '25e2aa0c0d0f2efeb739a87ae42543b5';
+
+const baseUrl = 'https://api.themoviedb.org/3/';
+
+export const getTrending = async () => {
+    const response = await axios.get(`${baseUrl}trending/all/week?api_key=${KEY}`);
+    return response.data;
+}
+
+//  życie identyfikatora filmu (movieID) w endpointach związanych z konkretnym filmem, takimi jak 
+// get - movie - details, get - movie - credits i get - movie - reviews, jest związane z konwencją używaną przez API themoviedb.org.
+export const getKeyword = async (keyword) => {
+ const response = axios.get(`${baseUrl}/search/movie?api_key=${apiKey}&query=${keyword}`);
+  return response.data.results;
+};
+
+export const getDetails = async (movieId) => {
+ const response = await axios.get(`${baseUrl}/movie/${movieId}?api_key=${apiKey}`);
+return response.data.results;
+};
+
+export const getCredits = async (movieId) => {
+   const response = await axios.get(`${baseUrl}/movie/${movieId}/credits?api_key=${apiKey}`);
+  return response.data.results;  
+}
+
+export const getReviews = async (movieId) => {
+   const response = axios.get(`${baseUrl}/movie/${movieId}/reviews?api_key=${apiKey}`);
+  return response.data.results;  
+}
+
+
+
